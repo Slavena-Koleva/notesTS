aa1c79d5fb261e41f49ca5641336c5c434b5b313
Slavena Koleva <slavena.koleva@hotmail.com>
2023-10-19 17:33:46 +0300

    Add tag creation

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -7,9 +7,11 @@ import { v4 } from "uuid";
 
 type NoteFormProps = {
   onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
 };
 
-export function NoteForm({ onSubmit }: NoteFormProps) {
+export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
@@ -37,13 +39,16 @@ export function NoteForm({ onSubmit }: NoteFormProps) {
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
               <CreatableReactSelect
-              onCreateOption= {label => {
-                const newTag = {id: v4(), label}
-                onAddTag(newTag)
-                setSelectedTags(prev => [...prev, newTag])
-              }}
+                onCreateOption={(label) => {
+                  const newTag = { id: v4(), label };
+                  onAddTag(newTag);
+                  setSelectedTags((prev) => [...prev, newTag]);
+                }}
                 value={selectedTags.map((tag) => {
                   return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map((t) => {
+                  return { label: t.label, value: t.id };
                 })}
                 onChange={(tags) => {
                   setSelectedTags(
